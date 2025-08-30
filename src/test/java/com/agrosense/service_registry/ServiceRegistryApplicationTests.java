097686e5a4e46b85d7bc01ddbc2807cd9bdc8870
devarshi-patidar1812 <devarshi.patidar18@gmail.com>
2025-08-30 23:47:52 +0530

    Render Setup

--- src/test/java/com/agrosense/service_registry/ServiceRegistryApplicationTests.java
+++ src/test/java/com/agrosense/service_registry/ServiceRegistryApplicationTests.java
@@ -3,10 +3,10 @@ package com.agrosense.service_registry;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class ServiceRegistryApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
